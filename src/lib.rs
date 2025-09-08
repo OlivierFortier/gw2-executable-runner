7d8771aaaefdb4d2de8812251c3879efc59396db
Olivier Fortier <olivier_fortier@outlook.com>
2025-09-08 12:30:27 -0400

    rename and update Gw2 Executable Loader to Gw2 Executable Runner with new paths and descriptions and update GH action build

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,12 @@ use nexus::AddonFlags;
 pub mod addon;
 
 nexus::export! {
-    name: "Gw2 Executable Loader",
+    name: "Gw2 Executable Runner",
     signature: -0x7A8B9C2A,
     load: addon::load,
     unload: addon::unload,
     flags: AddonFlags::None,
     provider: nexus::UpdateProvider::GitHub,
-    update_link: "https://github.com/OlivierFortier/gw2-executable-loader",
+    update_link: "https://github.com/OlivierFortier/gw2-executable-runner",
     log_filter: "trace"
 }
